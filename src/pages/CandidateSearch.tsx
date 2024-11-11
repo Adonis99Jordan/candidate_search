34e68a89de5da9b1c5216f2f3c9cbda78ffa5824
TukuNoni71 <Adonisjzepeda@gmail.com>
2024-11-11 08:53:27 -0500

    saved

--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { searchGithub, searchGithubUser } from '../api/API';
 import { Candidate } from '../interfaces/Candidate.interface';
 
